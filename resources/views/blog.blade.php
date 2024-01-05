3e196c0649d3d62090aa629970f5330bd860cdb5
Amdzak <ahmadzulfikar133@gmail.com>
2024-01-05 17:46:19 +0700

    edit background color

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -87,7 +87,7 @@
     </div>
 
     {{-- kontent --}}
-    <section class="bg-white dark:bg-gray-900">
+    <section class=" dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
                 <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
@@ -188,7 +188,7 @@
                 </article>
             </div>
         </div>
-      </section>
+    </section>
 
   </div>
 </div>
